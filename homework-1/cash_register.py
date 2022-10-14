0155884a1864c483f1ad1e96811be8a984856a8d
Lujein Nouelaty <lujein90@gmail.com>
2022-10-14 17:37:03 +0100

    Working on homework

--- homework-1/cash_register.py
+++ homework-1/cash_register.py
@@ -1,47 +1,80 @@
-"""
-
-TASK 1
-
-Write a class to represent a Cash Register.
-You class should keep the state of price total and purchased items
-
-Below is a starter code:
-------------------------
-1. you can add new variables and function if you want to
-2. you will NEED to add accepted method parameters where required.
-For example, method add_item probably accepts some kind of an item?..
-3. You will need to write some examples of how your code works.
-
-"""
+# """
+#
+# TASK 1
+# #
+# # Write a class to represent a Cash Register.
+# # Your class should keep the state of price total and purchased items
+# #
+# # Below is a starter code:
+# # ------------------------
+# # 1. you can add new variables and function if you want to
+# # 2. you will NEED to add accepted method parameters where required.
+# # For example, method add_item probably accepts some kind of an item?..
+# # 3. You will need to write some examples of how your code works.
+# #
+# # """
 
 
 class CashRegister:
 
-    def __init__(self):
-
-        self.total_items = None # {'item': 'price'}
-        self.total_price = 0
-        self.discount = 0
-
-    def add_item(self):
-        pass
-
-    def remove_item(self):
-        pass
-
-    def apply_discount(self):
-        pass
-
-    def get_total(self):
-        pass
-
-    def show_items(self):
-        pass
-
-    def reset_register(self):
-        pass
+    def __init__(self, item_name, price, in_stock=True):
+        self.total_items = []
+        self.total_price = +-1
+        self.discount = 0 if price else "20%"
+        self.item_name = item_name
+        self.price = price
+        self.in_stock = in_stock
+        self.total_items.append({"item_name": self.item_name, "price": self.price, "in_stock": self.stock})
 
 
-# EXAMPLE code run:
 
-# ADD
+    def add_items(self, new_item, new_price, in_stock):
+        self.total_items.append({"item_name": new_item, "price": new_price, "in_stock": in_stock})
+
+
+
+
+item_1 = CashRegister("Earrings", 165, True)
+print(item_1.append)
+
+
+
+
+
+
+
+
+
+
+#     def add_item(self, new_item, new_price):
+#         self.total_items.append({"item": new_item, "price": new_price, "stock": stock"})
+#     #     print(f"Your item {self.item_name} has been added to your basket.")
+#     #
+#     # def remove_item(self):
+#     #     print(f"Your item {self.item_name} has been removed from your basket")
+#     #
+#     # def apply_discount(self):
+#     #     print(f"A discount of {self.discount} has been applied to your basket.")
+#     #
+#     # def get_total(self, price):
+#     #     self.total_price += price
+#     #     return self.total_price
+#     #
+#     # def show_items(self, total_items):
+#     #
+#     #
+#     # def reset_register(self):
+#
+#
+#
+# item_1 = CashRegister("earrings", "£165")
+# print(item_1.total_item)
+# # my_shop.remove_item()
+# # my_shop.get_total(165)
+# # for total in sum(range(50):
+# # my_shop.apply_discount()
+#
+#
+#
+
+
--- homework-1/student.py
+++ homework-1/student.py
@@ -14,14 +14,36 @@ Software Student and Data Science student.
 
 class Student:
 
-    def __init__(self, name, age, id):
+    def __init__(self, name, age, id, tutor):
         self.name = name
         self.age = age
         self.id = id
         self.subjects = dict()
+        self.tutor = tutor
 
 
-# class CFGStudent(<should inherit from Student>)
-#     create new methods that manage student's subects (add/remove new subject and its graade to the dict)
+class CFGStudent:
+
+    def __init__(self, full_name, specialisation, season, mark=True):
+        self.full_name = full_name
+        self.specialisation = specialisation
+        self.season = season
+        self.mark = 70 if mark else 50
+
+    def add_subject(self):
+        print(f"{self.full_name} completed the {self.specialisation} cohort and achieved {self.mark}%.")
+
+    def remove_subject(self):
+        print(f"{self.full_name} was removed from the {self.specialisation} cohort because she was only getting {self.mark} in all her homework!")
+
+    # def view_all_subjects(self):
+
+student_1 = CFGStudent("Lujein Nouelaty", "Software", "Autumn", True)
+student_1.add_subject()
+# for student_mark in sum(70):
+#     student_1.remove_subject()
+
+
+#     create new methods that manage student's subjects (add/remove new subject and its grade to the dict)
 #     create a method to view all subjects taken by a student
 #     create a method  (and a new variable) to get student's overall mark (use average)
